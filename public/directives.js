abefdb7001235eb180edee6939d677d9c00390f1
Victorystick <victorystick@gmail.com>
2014-03-12 11:07:19 +0100

    Added lqCard directive.

--- public/directives.js
+++ public/directives.js
@@ -1,5 +1,15 @@
 angular.module('LazyQ')
 
+.directive('lqCard', function() {
+	return {
+		restrict: 'E',
+		scope: {
+			course: '=',
+		},
+		templateUrl: 'template/Card.html'
+	}
+})
+
 .directive('lqQueuecard', function() {
 	return {
 		restrict: 'E',
