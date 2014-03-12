a014519a55886db4376f9da04404221f5173d903
Victorystick <victorystick@gmail.com>
2014-03-12 09:41:09 +0100

    Added filter for lock icon in Search-view

--- public/filters.js
+++ public/filters.js
@@ -2,11 +2,20 @@ angular.module('LazyQ')
 
 .filter('ifactive', function() {
 	return function(courses, override) {
-		if (override) return courses
-		var out = [];
-		courses.forEach(function(i, val){
-			if (val.active) out.append(val);
-		})
-		return out;
-	}
-});
+		if (override) {
+			return courses;
+		}
+		return courses.filter(function (course) {
+			return course.active;
+		});
+	};
+})
+
+.filter('lockIcon', ['$sce', function ($sce) {
+	return function (bool) {
+		if (!bool) {
+			return $sce.trustAsHtml('<span class="glyphicon glyphicon-lock"></span>');
+		}
+		return '';
+	};
+}]);
